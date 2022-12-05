e4c60ac1cfebb71b4f4b5f6b60d3b0630ede65dd
james_li <>
2022-12-05 10:31:59 +0800

    modify users table migrate

--- database/migrations/2022_12_05_020352_create_users_table.php
+++ database/migrations/2022_12_05_020352_create_users_table.php
@@ -15,10 +15,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->dateTime('email_verified_at');
+            $table->string('password');
+            $table->string('remember_token');
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      *
